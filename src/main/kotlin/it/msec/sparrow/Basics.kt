42d947efc4b3fabae6c9e2ee366b5debe749a394
emacolombo <emanuele.colombo@lastminute.com>
2019-09-11 13:54:47 +0200

    Small fix

--- src/main/kotlin/it/msec/sparrow/Basics.kt
+++ src/main/kotlin/it/msec/sparrow/Basics.kt
@@ -1,6 +1,6 @@
 package it.msec.sparrow
 
-inline fun <A> identity(a: A) = a
+fun <A> identity(a: A) = a
 
 inline infix fun <A, B, C> ((B) -> C).compose(crossinline f: (A) -> B): (A) -> C = { a -> this(f(a)) }
 inline infix fun <A, B, C> ((A) -> B).andThen(crossinline f: (B) -> C): (A) -> C = { a -> f(this(a)) }
