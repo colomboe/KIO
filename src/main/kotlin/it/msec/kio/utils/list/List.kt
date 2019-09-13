cc8e3595d5113b2e0684ed37f137b0c832cbbf6a
emacolombo <emanuele.colombo@lastminute.com>
2019-09-13 19:56:17 +0200

    Implemented traverse on list and rewritten sequence using traverse

--- src/main/kotlin/it/msec/kio/utils/list/List.kt
+++ src/main/kotlin/it/msec/kio/utils/list/List.kt
@@ -1,6 +1,7 @@
 package it.msec.kio.utils.list
 
 import it.msec.kio.KIO
+import it.msec.kio.core.identity
 import it.msec.kio.failureEnv
 import it.msec.kio.internals.KIOInternals.evalAccessR
 import it.msec.kio.justEnv
@@ -8,10 +9,12 @@ import it.msec.kio.result.Failure
 import it.msec.kio.result.Success
 import it.msec.kio.runtime.unsafeRunSuspended
 
-fun <R, E, A> List<KIO<R, E, A>>.sequence(): KIO<R, E, List<A>> = evalAccessR { env ->
-    val results = map { it.unsafeRunSuspended(env) }
-    if (results.all { it is Success<A> }) {
-        justEnv(results.map { (it as Success<A>).value })
+fun <A, R, E, B> List<A>.traverse(f: (A) -> KIO<R, E, B>): KIO<R, E, List<B>> = evalAccessR { env ->
+    val results = map { f(it).unsafeRunSuspended(env) }
+    if (results.all { it is Success<B> }) {
+        justEnv(results.map { (it as Success<B>).value })
     } else
         failureEnv(results.filterIsInstance<Failure<E>>().map { it.error }.first())
 }
+
+fun <R, E, A> List<KIO<R, E, A>>.sequence(): KIO<R, E, List<A>> = traverse(::identity)
