9f8c4dbc1255755c70f9dfa6dfdbd9e55cc0c7a8
emacolombo <emanuele.colombo@lastminute.com>
2019-09-16 22:03:04 +0200

    Improved test coverage (WIP)

---
+++ src/test/kotlin/it/msec/kio/ref/RefTest.kt
@@ -0,0 +1,40 @@
+package it.msec.kio.ref
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import it.msec.kio.flatMap
+import it.msec.kio.runtime.unsafeRunSyncAndGet
+import org.junit.Test
+
+class RefTest {
+
+    @Test
+    fun `ref provides its value`() {
+        val ref = Ref(33)
+        val r = ref.get().unsafeRunSyncAndGet()
+        assertThat(r).isEqualTo(33)
+    }
+
+    @Test
+    fun `ref value can be set`() {
+        val ref = Ref(33)
+        val r = ref.set(44).flatMap { ref.get() }.unsafeRunSyncAndGet()
+        assertThat(r).isEqualTo(44)
+    }
+
+    @Test
+    fun `ref value can be retrieved and updated atomically`() {
+        val ref = Ref(33)
+        val r = ref.getAndUpdate { it * 2 }.unsafeRunSyncAndGet()
+        val r2 = ref.get().unsafeRunSyncAndGet()
+        assertThat(r).isEqualTo(33)
+        assertThat(r2).isEqualTo(66)
+    }
+
+    @Test
+    fun `ref value can be updated and then retrieved atomically`() {
+        val ref = Ref(33)
+        val r = ref.updateAndGet { it * 2 }.unsafeRunSyncAndGet()
+        assertThat(r).isEqualTo(66)
+    }
+}
