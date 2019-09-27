e9974e509a818bb9d5b8de21b452345d1fd9b7f7
emacolombo <emanuele.colombo@lastminute.com>
2019-09-27 19:30:42 +0200

    Better generic types naming

--- src/main/kotlin/it/msec/kio/KIO.kt
+++ src/main/kotlin/it/msec/kio/KIO.kt
@@ -6,7 +6,8 @@ sealed class KIO<in R, out E, out A>
 data class Eager<R, E, A>(val value: Result<E, A>): KIO<R, E, A>()
 data class Lazy<R, E, A>(val valueF: suspend () -> Result<E, A>): KIO<R, E, A>()
 data class EnvAccess<R, E, A>(val accessF: suspend (R) -> KIO<R, E, A>): KIO<R, E, A>()
-data class FlatMap<R, E, B, L, A>(val flatMapF: suspend (Result<E, A>) -> KIO<R, L, B>, val prev: KIO<R, E, A>): KIO<R, L, B>()
+data class FlatMap<R, E, A, L, B>(val flatMapF: suspend (Result<L, B>) -> KIO<R, E, A>, val prev: KIO<R, L, B>): KIO<R, E, A>()
+data class DoFlatMap<R, E, A, L, B>(val flatMapF: suspend (Result<L, B>) -> KIO<R, E, A>, val prev: KIO<R, L, B>): KIO<R, E, A>()
 
 typealias BIO<E, A> = KIO<Any, E, A>
 typealias TaskR<R, A> = KIO<R, Nothing, A>
