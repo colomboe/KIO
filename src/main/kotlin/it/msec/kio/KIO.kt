d15c630d06ded3099faff7133dea210918c8f906
emacolombo <emanuele.colombo@lastminute.com>
2019-09-27 19:31:32 +0200

    Reverted bad commit...

--- src/main/kotlin/it/msec/kio/KIO.kt
+++ src/main/kotlin/it/msec/kio/KIO.kt
@@ -7,7 +7,6 @@ data class Eager<R, E, A>(val value: Result<E, A>): KIO<R, E, A>()
 data class Lazy<R, E, A>(val valueF: suspend () -> Result<E, A>): KIO<R, E, A>()
 data class EnvAccess<R, E, A>(val accessF: suspend (R) -> KIO<R, E, A>): KIO<R, E, A>()
 data class FlatMap<R, E, A, L, B>(val flatMapF: suspend (Result<L, B>) -> KIO<R, E, A>, val prev: KIO<R, L, B>): KIO<R, E, A>()
-data class DoFlatMap<R, E, A, L, B>(val flatMapF: suspend (Result<L, B>) -> KIO<R, E, A>, val prev: KIO<R, L, B>): KIO<R, E, A>()
 
 typealias BIO<E, A> = KIO<Any, E, A>
 typealias TaskR<R, A> = KIO<R, Nothing, A>
