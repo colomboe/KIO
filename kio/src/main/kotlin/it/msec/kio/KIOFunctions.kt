f08a4482ec126cac7ad50485cc973ed690082884
emacolombo <emanuele.colombo@lastminute.com>
2019-10-09 22:40:45 +0200

    Some more functions regarding ref and ask

--- kio/src/main/kotlin/it/msec/kio/KIOFunctions.kt
+++ kio/src/main/kotlin/it/msec/kio/KIOFunctions.kt
@@ -64,6 +64,10 @@ inline fun <R, A> askPure(crossinline f: (R) -> A): URIO<R, A> = doAskR { justR(
 
 fun <R> askPure(): URIO<R, R> = doAskR(::justR)
 
+inline fun <R, E, A> withR(crossinline f: R.() -> KIO<R, E, A>): KIO<R, E, A> = doAskR { f(it) }
+
+inline fun <R, A> withPureR(crossinline f: R.() -> A): URIO<R, A> = doAskR { justR(f(it)) }
+
 fun <R, E, A, B> KIO<R, E, A>.map(f: (A) -> B): KIO<R, E, B> = doSuccessMap(f)
 
 inline fun <R, E, A, B> KIO<R, E, A>.flatMap(crossinline f: (A) -> KIO<R, E, B>): KIO<R, E, B> = doFlatMap {
--- kio/src/main/kotlin/it/msec/kio/ref/Ref.kt
+++ kio/src/main/kotlin/it/msec/kio/ref/Ref.kt
@@ -1,6 +1,7 @@
 package it.msec.kio.ref
 
 import it.msec.kio.UIO
+import it.msec.kio.common.tuple.T2
 import it.msec.kio.effect
 import java.util.concurrent.atomic.AtomicReference
 
@@ -15,6 +16,19 @@ data class Ref<A> private constructor(private val value: AtomicReference<A>) {
     fun getAndUpdate(f: (A) -> A): UIO<A> = effect { value.getAndUpdate(f) }
 
     fun updateAndGet(f: (A) -> A): UIO<A> = effect { value.updateAndGet(f) }
+
+    fun <B> modify(f: (A) -> T2<A, B>): UIO<B> {
+
+        tailrec fun applyF(): B {
+            val currentValue = value.get()
+            val (updatedValue, output) = f(currentValue)
+            return if (value.compareAndSet(currentValue, updatedValue)) output else applyF()
+        }
+
+        return effect { applyF() }
+    }
+
+    fun update(f: (A) -> A): UIO<Unit> = modify { a -> T2(f(a), Unit) }
 
 }
 
