8411a3badf2e2b6c7f10f34437d114bc9ed84844
emacolombo <emanuele.colombo@lastminute.com>
2019-10-14 22:08:22 +0200

    Aligned also the coroutine-based runtime

--- kio/src/main/kotlin/it/msec/kio/runtime/RuntimeSuspended.kt
+++ kio/src/main/kotlin/it/msec/kio/runtime/RuntimeSuspended.kt
@@ -30,17 +30,18 @@ object RuntimeSuspended : KIORuntime {
             execute(kio, env)
 
     @Suppress("UNCHECKED_CAST")
-    private suspend fun <R, E, A> CoroutineScope.execute(k: KIO<R, E, A>, r: R): Result<E, A> {
+    private suspend fun <R, E, A> CoroutineScope.execute(k: KIO<R, E, A>, initialR: R): Result<E, A> {
 
         val stack = RuntimeStack()
 
+        var r: Any? = initialR
         var current: Any = k
         while (true) {
             current = when (current) {
                 is Eager<*, *, *> -> current.value
                 is Lazy<*, *, *> -> try { current.valueF() } catch(t: Throwable) { Failure(t) }
                 is LazySuspended<*, *, *> -> try { current.suspendedF(this) } catch(t: Throwable) { Failure(t) }
-                is AskR<*, *, *> -> (current.accessF as (R) -> KIO<R, *, *>)(r)
+                is AskR<*, *, *> -> (current.accessF as (R) -> KIO<R, *, *>)(r as R)
                 is SuccessMap<*, *, *, *> -> {
                     stack.push(successMapToF(current))
                     current.prev
@@ -53,6 +54,16 @@ object RuntimeSuspended : KIORuntime {
                 is Result<*, *> -> {
                     val fn = stack.pop()
                     if (fn != null) fn(current) else return current as Result<E, A>
+                }
+                is ProvideR<*, *, *> -> {
+                    val prevR = r
+                    stack.push { result -> RestoreR(prevR, result) }
+                    r = current.r
+                    current.prev
+                }
+                is RestoreR<*, *, *> -> {
+                    r = current.r
+                    current.value
                 }
                 else -> throw NeverHereException
             }
