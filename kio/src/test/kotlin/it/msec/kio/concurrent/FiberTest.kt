223e61ffb13cabd9fcab4caa696f6413aff96017
emacolombo <emanuele.colombo@lastminute.com>
2019-10-27 10:59:50 +0100

    Testing fibers with comprehension... doesn't work...

--- kio/src/test/kotlin/it/msec/kio/concurrent/FiberTest.kt
+++ kio/src/test/kotlin/it/msec/kio/concurrent/FiberTest.kt
@@ -2,6 +2,7 @@ package it.msec.kio.concurrent
 
 import it.msec.kio.*
 import it.msec.kio.common.tuple.T
+import it.msec.kio.result.get
 import it.msec.kio.runtime.RuntimeSuspended
 import kotlinx.coroutines.delay
 import org.junit.Test
@@ -45,5 +46,44 @@ class FiberTest {
         val result = RuntimeSuspended.unsafeRunSyncAndGet(prog)
         println(result)
 
+    }
+
+    @Test
+    fun `fibers can be launched (with comprehension)`() {
+
+        val first = ask { r: Int ->
+            suspendedR<Int, String> {
+                println("first start")
+                delay(5000)
+                println("first end")
+                "first $r (${Thread.currentThread().name})"
+            }
+        }
+
+        val second = ask { r: Int ->
+            suspendedR<Int, String> {
+                println("second start")
+                delay(5000)
+                println("second end")
+                "second $r (${Thread.currentThread().name})"
+            }
+        }
+
+        val prog: URIO<Int, Unit> = binding {
+            effect { println("Result:") }.bind()
+            val fiber1 by +first.fork()
+            val fiber2 by +second.fork()
+            val r1 by +fiber1.await()
+            val r2 by +fiber2.await()
+
+            effect {
+                println(r1)
+                println(r2)
+                println("done")
+            }.bind()
+        }
+
+        val result = RuntimeSuspended.unsafeRunSync(prog, 33).get()
+        println(result)
     }
 }
