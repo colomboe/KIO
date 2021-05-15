0d0ad550db762e21299e86887519dc230f96e11d
emacolombo <emanuele.colombo@lastminute.com>
2021-05-15 22:19:21 +0200

    Updated dependencies and moved to kotlin 1.5

--- kio/src/main/kotlin/it/msec/kio/Fiber.kt
+++ kio/src/main/kotlin/it/msec/kio/Fiber.kt
@@ -3,6 +3,7 @@ package it.msec.kio
 import it.msec.kio.result.Result
 import kotlinx.coroutines.Deferred
 
-inline class Fiber<E, A>(val deferred: Deferred<Result<E, A>>)
+@JvmInline
+value class Fiber<E, A>(val deferred: Deferred<Result<E, A>>)
 
 
