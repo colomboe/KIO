48245f5950bdc871bc23fdd3f0ef08c1a7b29e96
emacolombo <emanuele.colombo@lastminute.com>
2021-05-15 22:25:19 +0200

    Fixed test

--- kio/src/main/kotlin/it/msec/kio/Option.kt
+++ kio/src/main/kotlin/it/msec/kio/Option.kt
@@ -1,7 +1,5 @@
 package it.msec.kio
 
-import it.msec.kio.common.nullable.then
-
 object Empty
 
 fun <A> empty(): Option<A> = failure(Empty)
@@ -9,6 +7,6 @@ fun <A> empty(): Option<A> = failure(Empty)
 fun <A> Option<A>.filter(f: (A) -> Boolean): Option<A> =
         flatMap { a -> if (f(a)) just(a) else failure(Empty) }
 
-fun <A> A?.toOption(): Option<A> = then(::just) ?: empty()
+fun <A> A?.toOption(): Option<A> = this?.let(::just) ?: empty()
 
 fun <E, A> KIO<Any, E, A>.toOption(): Option<A> = mapError { Empty }
